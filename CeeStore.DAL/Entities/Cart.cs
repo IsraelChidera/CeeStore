bb378417ebabcd4015fc3c5930008878bd3f3d66
IsraelChidera <israelchidera54@gmail.com>
2023-04-17 13:03:42 +0100

    Feature: Added cart entity

--- CeeStore.DAL/Entities/Cart.cs
+++ CeeStore.DAL/Entities/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace CeeStore.DAL.Entities
     public class Cart
     {
         public Guid CartId { get; set; }
+
+        [ForeignKey(nameof(AppUser))]
         public Guid UserId { get; set; }
         public AppUser User { get; set; }
         public ICollection<CartItem> CartItems { get; set; }
