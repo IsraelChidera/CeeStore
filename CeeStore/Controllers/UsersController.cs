3109afe20821c310e84d833c4b130f2b73be2a5a
IsraelChidera <israelchidera54@gmail.com>
2023-04-21 20:23:07 +0100

    Added User Controller

---
+++ CeeStore/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using CeeStore.DAL.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CeeStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet("get-all-users")]
+        [Authorize(Roles = "SuperAdmin, Admin")]
+        public async Task<IEnumerable<AppUser>> GetAllUsers()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+
+    }
+}
